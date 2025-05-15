4cd3b720a0216769810b9885ad2b3d77065a1a77
DSM2 <fatec-dsm2@academico.intra>
2025-05-14 21:32:33 -0300

    Atualização com CRUD

--- controllers/ClientesController.js
+++ controllers/ClientesController.js
@@ -23,4 +23,41 @@ router.post("/clientes/new", (req,res) => {
   })
 })
 
+router.post("/clientes/update/:id", (req,res) => {
+  const id = req.body.id
+  const nome = req.body.nome
+  const cpf = req.body.cpf
+  const endereco = req.body.endereco
+  Cliente.update({
+    nome : nome,
+    cpf : cpf,
+    endereco : endereco
+   },
+   {where: {id : id}}
+  ).then(() => {
+    res.redirect("/clientes")
+  })
+}) 
+
+router.get("/clientes/delete/:id", (req, res) => {
+  const id = req.params.id
+  Cliente.destroy({
+    where: {
+      id : id
+    }
+  }).then(() => {
+    res.redirect("/clientes")
+  })
+})
+
+
+router.get("/clientes/edit/:id", (req, res) => {
+  const id = req.params.id
+  Cliente.findByPk(id).then(function(cliente) {
+    res.render("clienteEdit", {
+      cliente : cliente
+    })
+  })
+})
+
 export default router;
